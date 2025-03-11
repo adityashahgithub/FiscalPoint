5fb8d97a8805f8bde1395d2ac01366df6940ee2f
SuzzanPatel <suzzanpatel10@gmail.com>
2025-03-11 20:54:48 +0530

    linegraph

---
+++ Frontend/linegraph1.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Expense Dashboard</title>
+    <link rel="stylesheet" href="css/dashboard.css">
+</head>
+<body>
+    
+    <aside class="sidebar">
+        <div class="profile">
+            <img src="css/profile.png" alt="Profile Image" class="avatar">
+        </div>
+        <ul class="menu">
+            <li><p> <span style="font-size: 20px;">Name</span></p></li>
+            <li> <a href="dashboard.html">Dashboard</a></li><br>
+            <li> <a href="setbudget.php">Budget</a></li><br>
+            <li> <a href="addexpense.php">Add Expense </a></li><br>
+            <li> <a href="linegraph.php">Line Graph Report </a></li><br>
+            <li> <a href="linegraph.php">Pie Graph Report </a></li><br>
+            <li> <a href="profile.html">Profile</a></li><br>
+            <li> <a href="logout.php">Logout</a></li><br>
+        </ul>
+    </aside>
+</body>
