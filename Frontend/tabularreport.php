425bebafd755ebfd8b1400bf6359b0043dc101b8
adityashahgithub <shahaditya29904@gmail.com>
2025-04-05 06:09:46 +0530

    tbr done

--- Frontend/tabularreport.php
+++ Frontend/tabularreport.php
@@ -98,7 +98,7 @@ $conn->close();
                     <li><a href="tabularreport.php"><i class="fas fa-list-alt"></i> All Expenses</a></li>
                     <li><a href="categorywisereport.php"><i class="fas fa-layer-group"></i> Category-wise Expense</a></li>
                 </ul>
-            </li><br>
+        </li><br>
             <li><a href="predictions.php"><i class="fas fa-robot"></i> <strong>Predictions</strong></a></li><br>
             <li><a href="profile.php"><i class="fas fa-user"></i> <strong>Profile</strong></a></li><br>
             <li><a href="logout.php"><i class="fas fa-sign-out-alt"></i> <strong>Logout</strong></a></li><br>
